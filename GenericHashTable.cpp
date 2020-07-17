c5e8a63636285ed8d846c3d518ca1713754cbe36
Alexander Agathos <alexander.agathos@gmail.com>
2020-07-17 20:47:18 +0300

    Initiale exercise from Marios

---
+++ GenericHashTable.cpp
@@ -0,0 +1,12 @@
+//
+// Created by marios on 26/5/20.
+//
+
+#include <cstring>
+#include "PatientEntry.h"
+#include "GenericlLinkedList.h"
+#include "GenericHashTable.h"
+
+using namespace exercise3namespace;
+
+
